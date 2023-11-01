8998bdba248812128a1800812336a073d3ef7bb7
Valentina <valentinavpsfacul@gmail.com>
2023-11-01 00:22:52 -0300

    jogo.h continuacao

--- include/Jogo/Jogo.h
+++ include/Jogo/Jogo.h
@@ -23,3 +23,25 @@ Game();
  * @param numero O número do jogador (1 ou 2).
  */
 void setTabuleiro(const Tabuleiro& jogador, int numero);
+
+/**
+ * @brief Obtém o tabuleiro do jogo de um jogador específico.
+ * @param numero O número do jogador (1 ou 2).
+ * @return O tabuleiro do jogador especificado.
+ */
+Tabuleiro& getTabuleiro(int numero);
+
+/**
+ * @brief Salva o estado atual do jogo em um arquivo.
+ * @param filename O nome do arquivo para salvar o jogo.
+ */
+void salvarJogo(std::string filename);
+
+/**
+ * @brief Carrega um jogo previamente salvo de um arquivo.
+ * @param filename O nome do arquivo para carregar o jogo.
+ */
+void carregarJogo(std::string filename);
+};
+
+#endif
