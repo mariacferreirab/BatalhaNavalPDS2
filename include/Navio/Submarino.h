7bf8f3b17b1580e4b08f0dbf6b6b087220a81b54
Valentina <valentinavpsfacul@gmail.com>
2023-11-01 00:13:17 -0300

    user story

---
+++ include/Navio/Submarino.h
@@ -0,0 +1,17 @@
+#ifndef SUBMARINO_H
+#define SUBMARINO_H
+
+#include "Navio.h"
+
+/**
+ * @brief Classe que representa um submarino, um tipo de navio do jogo de batalha naval.
+ */
+class Submarino : public Navio {
+public:
+    /**
+     * @brief Construtor padrão da classe Submarino.
+     */
+    Submarino();
+};
+
+#endif 
