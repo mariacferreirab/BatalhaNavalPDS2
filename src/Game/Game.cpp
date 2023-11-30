a1fc2bd35d7e76dad7922eeda9f9c626b21ffb57
MarianaVRabelo <marivillefortr@gmail.com>
2023-11-30 10:38:52 -0300

    Update and rename Jogo.cpp to Game.cpp

---
+++ src/Game/Game.cpp
@@ -0,0 +1,82 @@
+#include "Game.hpp"
+
+Game::Game()
+{
+}
+
+void Game::setTabuleiro(const Tabuleiro& jogador, int numero)
+{
+    if (numero == 1)
+    {
+        jogador1 = jogador;
+    }
+    else
+    {
+        jogador2 = jogador;
+    }
+}
+
+Tabuleiro& Game::getTabuleiro(int numero)
+{
+    if (numero == 1)
+    {
+        return jogador1;
+    }
+    else
+    {
+        return jogador2;
+    }
+}
+void Game::salvarJogo(std::string filename)
+{
+    std::ofstream file(filename);
+    if (file.is_open())
+    {
+        // Salvar jogador1
+        file << jogador1.getNome() << std::endl;
+        std::vector<Navio*> navios = jogador1.getNavios();
+        for (auto& navio : navios)
+        {
+            file << navio->getNome() << " " << navio->getTamanho() << " " << navio->getAfundou() << std::endl;
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                file << jogador1.getTabuleiro()[i][j] << " ";
+            }
+            file << std::endl;
+        }
+        // Salvar jogador2
+        file << jogador2.getNome() << std::endl;
+        navios = jogador2.getNavios();
+        for (auto& navio : navios)
+        {
+            file << navio->getNome() << " " << navio->getTamanho() << " " << navio->getAfundou() << std::endl;
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                file << jogador2.getTabuleiro()[i][j] << " ";
+            }
+            file << std::endl;
+        }
+
+        file.close();
+    }
+}
+
+void Game::carregarJogo(std::string filename)
+{
+    std::ifstream file(filename);
+    if (file.is_open())
+    {
+        std::string nome1, nome2;
+        file >> nome1 >> nome2;
+        jogador1.setNome(nome1);
+        jogador2.setNome(nome2);
+        // carregar outras variaveis do jogo
+        file.close();
+    }
+}
